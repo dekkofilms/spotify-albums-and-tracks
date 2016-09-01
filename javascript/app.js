8681aa5dd303c3e1e3f3571764d4b9656cf180d6
Taylor King <dekkofilms@gmail.com>
2016-09-01 15:15:59 -0500

    fully functional

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
 // Self envoking function! once the document is ready, bootstrap our application.
-// We do this to make sure that all the HTML is rendered before we do things 
-// like attach event listeners and any dom manipulation.  
+// We do this to make sure that all the HTML is rendered before we do things
+// like attach event listeners and any dom manipulation.
 (function(){
   $(document).ready(function(){
     bootstrapSpotifySearch();
@@ -27,7 +27,7 @@ function bootstrapSpotifySearch(){
           url: searchUrl
       });
 
-      // Attach the callback for success 
+      // Attach the callback for success
       // (We could have used the success callback directly)
       spotifyQueryRequest.done(function (data) {
         var artists = data.artists;
@@ -35,7 +35,7 @@ function bootstrapSpotifySearch(){
         // Clear the output area
         outputArea.html('');
 
-        // The spotify API sends back an arrat 'items' 
+        // The spotify API sends back an arrat 'items'
         // Which contains the first 20 matching elements.
         // In our case they are artists.
         artists.items.forEach(function(artist){
@@ -47,7 +47,7 @@ function bootstrapSpotifySearch(){
         })
       });
 
-      // Attach the callback for failure 
+      // Attach the callback for failure
       // (Again, we could have used the error callback direcetly)
       spotifyQueryRequest.fail(function (error) {
         console.log("Something Failed During Spotify Q Request:")
@@ -60,7 +60,43 @@ function bootstrapSpotifySearch(){
 function displayAlbumsAndTracks(event) {
   var appendToMe = $('#albums-and-tracks');
 
-  // These two lines can be deleted. They're mostly for show. 
+  var artistId = $(event.target).attr('data-spotify-id');
+
+  var getAlbums = $.ajax({
+                    url: 'https://api.spotify.com/v1/artists/' + artistId + '/albums',
+                    dataType: 'json'
+                })
+
+  var albumArray = [];
+
+  getAlbums.done(function (data) {
+    data.items.forEach(function (album) {
+
+      var albumId = album.id
+      var albumName = album.name
+
+      var getSingleAlbum = $.ajax({
+        url: 'https://api.spotify.com/v1/albums/' + albumId
+      })
+
+      var getAllTracks = $.ajax({
+        url: 'https://api.spotify.com/v1/albums/' + albumId + '/tracks'
+      })
+
+      getAllTracks.done(function (tracks) {
+        getSingleAlbum.done(function (data) {
+          var albumYear = data.release_date;
+          appendToMe.append('<h3>' + albumName + ' ' + albumYear + '</h3>');
+          tracks.items.forEach(function (trackName) {
+            appendToMe.append('<li>' + trackName.name + '</li>');
+        })
+        })
+      })
+    })
+  })
+
+
+  // These two lines can be deleted. They're mostly for show.
   console.log("you clicked on:");
   console.log($(event.target).attr('data-spotify-id'));//.attr('data-spotify-id'));
 }
